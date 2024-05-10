5e9b303c2b2bad2896ba4e4ec306be991627c83a
ezgisubasii <120321455+ezgisubasii@users.noreply.github.com>
2024-05-10 17:14:54 +0300

    add file

---
+++ accordion.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    // Initialize jQuery UI Accordion
+    $("#accordion").accordion({
+        collapsible: true, // Allow all sections to be collapsible
+        active: false, // Start with all sections closed
+        heightStyle: "content" // Adjust the height of the accordion panels based on content
+    });
+});
